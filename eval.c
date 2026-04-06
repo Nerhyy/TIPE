97986c759d02126ece5f728a9494d9ea5a0fc06b
Nerhyy <neji.nguyen005@gmail.com>
2026-04-06 18:35:57 +0200

    tentative de fix qui marche pas

--- eval.c
+++ eval.c
@@ -963,19 +963,13 @@ int eval_endgame(chessboard* cb){
 int eval(chessboard* cb){
 
 
-    if(cb->phase > 17){
-        int final_score = eval_midgame(cb); //Eval midgame
-        return (cb->turn == WHITE) ? final_score : -final_score;
-    }
-    else{
-        int phase = cb->phase; //On récupère la phase entre 0 et 24
+    int phase = cb->phase; //On récupère la phase entre 0 et 24
 
-        int score_mg = eval_midgame(cb); //Eval midgame
-        int score_eg = eval_endgame(cb); //Eval endgame
+    int score_mg = eval_midgame(cb); //Eval midgame
+    int score_eg = eval_endgame(cb); //Eval endgame
 
-        int final_score = ((score_mg*phase)+(score_eg*(24-phase)))/24; //On interpole
+    int final_score = ((score_mg*phase)+(score_eg*(24-phase)))/24; //On interpole
 
-        return (cb->turn == WHITE) ? final_score : -final_score;
-    }
+    return final_score;
 
 }
